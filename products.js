2099c91b12396627a845c1735d43d965058546b5
eksa-arifa <eksaarifa@gmail.com>
2024-04-17 08:57:21 +0700

    first commit

---
+++ products.js
@@ -0,0 +1,65 @@
+const products = [
+    {
+        id:1,
+        name:'Batik',
+        img:'batik/Picture.png',
+        price: 'Rp.1.500.000,00',
+        terjual: '367'
+    },
+    {
+        id:2,
+        name:'Batik',
+        img:'batik/Picture(1).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '983'
+    },
+    {
+        id:3,
+        name:'Batik',
+        img:'batik/Picture(2).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '3045'
+    },
+    {
+        id:4,
+        name:'Batik',
+        img:'batik/Picture(3).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '993'
+    },
+    {
+        id:5,
+        name:'Batik',
+        img:'batik/Picture(4).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '1043'
+    },
+    {
+        id:6,
+        name:'Batik',
+        img:'batik/Picture(5).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '348'
+    },
+    {
+        id:7,
+        name:'Batik',
+        img:'batik/Picture(6).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '767'
+    },
+    {
+        id:8,
+        name:'Batik',
+        img:'batik/Picture(7).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '654'
+    },
+    {
+        id:9,
+        name:'Batik',
+        img:'batik/Picture(8).png',
+        price: 'Rp.1.500.000,00',
+        terjual: '2045'
+    },
+]
